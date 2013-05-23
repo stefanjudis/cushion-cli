6d3b9633ffa5317dd1562337db52ace349a8283e
stefan judis <stefanjudis@gmail.com>
2013-05-23 22:54:16 +0200

    added to more tests help._generalHelp and help._specificHelp

--- test/help/helpTest.js
+++ test/help/helpTest.js
@@ -2,15 +2,18 @@
 
 module.exports = {
   setUp: function(callback) {
+    // block console.log for cushion output
+    this._console = console.log;
+    console.log = function(){};
+
     this.help = require('../../lib/help/help');
+    this.messages = require('../../lib/help/messages');
 
     callback();
   },
 
 
   tearDown: function(callback) {
-    console.log = this._console;
-
     callback();
   },
 
@@ -20,7 +23,8 @@ module.exports = {
       var cli = {},
           help = this.help,
           input = ['help'],
-          levelObject = {};
+          levelObject = {},
+          _generalHelp = help._generalHelp;
 
       this.help._generalHelp = function(object, inputCli) {
         test.strictEqual(arguments.length, 2);
@@ -31,12 +35,15 @@ module.exports = {
       };
 
       help.getHelp(levelObject, input, cli);
+
+      this.help._generalHelp = _generalHelp
     },
     twoArguments: function(test) {
       var cli = {},
           help = this.help,
           input = ['help', 'secondParameter'],
-          levelObject = {};
+          levelObject = {},
+          _specificHelp = help._specificHelp;
 
       this.help._specificHelp = function(args, inputCli) {
         test.strictEqual(arguments.length, 2);
@@ -48,6 +55,77 @@ module.exports = {
       };
 
       help.getHelp(levelObject, input, cli);
+
+      this.help._specificHelp = _specificHelp;
+    }
+  },
+
+
+  generalHelp: function(test) {
+    var cli = {},
+        help = this.help,
+        messages = this.messages,
+        object = {
+      _test: '_test',
+      __test : '__test',
+      _simpleCommand: '_simpleCommand'
+    };
+
+    console.log = function(message) {
+      test.strictEqual(typeof message, 'string');
+      test.strictEqual(arguments.length, 1);
+      test.strictEqual(message, messages.general);
+    }
+
+    console.pretty = function(commands) {
+      test.strictEqual(typeof commands, 'object');
+      test.strictEqual(commands instanceof Array, true);
+      test.strictEqual(commands.length, 1);
+      test.strictEqual(commands[0], 'test');
+    };
+
+    cli.prompt = function() {
+      test.done();
+    }
+
+    help._generalHelp(object, cli);
+  },
+
+
+  specificHelp: {
+    knownCommand: function(test) {
+      var cli = {},
+          help = this.help,
+          input = ['config'],
+          messages = this.messages;
+
+      console.log = function(message) {
+        test.strictEqual(typeof message, 'string');
+        test.strictEqual(arguments.length, 1);
+        test.strictEqual(message, messages.config)
+      }
+
+      cli.prompt = function() {
+        test.done();
+      }
+
+      help._specificHelp(input, cli);
+    },
+    unknownCommand: function(test) {
+      var cli = {},
+          help = this.help,
+          input = ['someCommand'];
+
+      console.log = function(message) {
+        test.strictEqual(typeof message, 'string');
+        test.strictEqual(arguments.length, 1);
+      }
+
+      cli.prompt = function() {
+        test.done();
+      }
+
+      help._specificHelp(input, cli);
     }
   }
 };
