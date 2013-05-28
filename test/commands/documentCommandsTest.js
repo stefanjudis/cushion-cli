2b02a0036b8ba0e832a835459e18ca99669490d7
stefan judis <stefanjudis@gmail.com>
2013-05-28 23:02:57 +0200

    added first test on document level

---
+++ test/commands/documentCommandsTest.js
@@ -0,0 +1,79 @@
+'use strict';
+
+module.exports = {
+  setUp: function(callback) {
+    // block console.log for cushion output
+    this._console = console.log;
+    console.log = function(){};
+
+    this.cli = require('../../lib/cliRunner');
+    this.cli.level = 'document';
+
+    callback();
+  },
+
+
+  tearDown: function(callback) {
+    console.log = this._console;
+
+    callback();
+  },
+
+
+  content: {
+    bodyResponseIsObject: {
+      withoutKeys: function(test) {
+        var cli = this.cli,
+            input = ['content', 'foo'];
+
+        cli.doc.body = function() {
+          test.strictEqual(arguments.length, 1);
+          test.strictEqual(typeof arguments[0], 'string');
+          test.strictEqual(arguments[0], 'foo');
+
+          return {};
+        }
+
+        console.log = function(message) {
+          test.strictEqual(arguments.length, 1);
+          test.strictEqual(typeof message, 'string')
+        };
+
+        cli.prompt = function() {
+          test.done();
+        };
+
+        cli.documentCommands._content(input, cli);
+      },
+      withKeys: function(test) {
+        var cli = this.cli,
+            input = ['content', 'foo'];
+
+        cli.doc.body = function() {
+          test.strictEqual(arguments.length, 1);
+          test.strictEqual(typeof arguments[0], 'string');
+          test.strictEqual(arguments[0], 'foo');
+
+          return {
+            foo: 'bar'
+          };
+        }
+
+        cli.documentCallbacks.content = function() {
+          test.strictEqual(arguments.length, 2);
+
+          test.strictEqual(arguments[0], false);
+
+          test.strictEqual(typeof arguments[1], 'object');
+          test.strictEqual(Object.keys(arguments[1]).length, 1);
+          test.strictEqual(arguments[1].foo, 'bar')
+
+          test.done();
+        };
+
+        cli.documentCommands._content(input, cli);
+      }
+    }
+  }
+
+};
