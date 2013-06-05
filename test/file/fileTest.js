237877bf93173aae8c16cac7565587c1e50d976e
stefan judis <stefanjudis@gmail.com>
2013-06-05 18:50:41 +0200

    default connections are saved in home directory now

--- test/file/fileTest.js
+++ test/file/fileTest.js
@@ -77,28 +77,81 @@ module.exports = {
   },
 
   writeConnections: function(test) {
-    var callbackFunction = function() {},
-        connections = {
+    var connections = {
       connection1: 'someData'
     },
-        writeFile = this.fs.writeFile;
+        callback = function() {},
+        exists = this.fs.exists;
 
-    this.fs.writeFile = function() {
-      test.strictEqual(arguments.length, 3);
+    this.fs.exists = function() {
+      test.strictEqual(arguments.length, 2);
 
       test.strictEqual(typeof arguments[0], 'string');
 
-      test.strictEqual(typeof arguments[1], 'string');
-      test.strictEqual(arguments[1], '{"connection1":"someData"}');
+      test.strictEqual(typeof arguments[1], 'function');
+      test.strictEqual(
+        arguments[1], this.cFile._writeConnectionsExistsCallback
+      );
 
-      test.strictEqual(typeof arguments[2], 'function');
-      test.strictEqual(arguments[2], callbackFunction);
+      test.strictEqual(typeof this.cFile.connections, 'object');
+      test.strictEqual(this.cFile.connections, connections);
+
+      test.strictEqual(typeof this.cFile.writeConnectionsCallback, 'function');
+      test.strictEqual(this.cFile.writeConnectionsCallback, callback);
 
       test.done();
+    }.bind(this);
+
+    this.cFile._writeConnections(connections, callback);
+
+    this.fs.exists = exists;
+  },
+
+
+  writeConnectionsExistsCallback: {
+    doesExist: function(test) {
+      var writeFile = this.fs.writeFile;
+
+      this.cFile.connections = {
+        connection1: 'someConnection'
+      };
+
+      this.fs.writeFile = function() {
+        test.strictEqual(arguments.length, 3);
+
+        test.strictEqual(typeof arguments[0], 'string');
+
+        test.strictEqual(typeof arguments[1], 'string');
+        test.strictEqual(arguments[1], '{"connection1":"someConnection"}');
+
+        test.strictEqual(typeof arguments[2], 'function');
+        test.strictEqual(arguments[2], this.cFile.writeConnectionsCallback);
+
+        test.done();
+      }.bind(this);
+
+      this.cFile._writeConnectionsExistsCallback(true);
+
+      this.fs.writeFile = writeFile;
+    },
+    doesNotExist: function(test) {
+      var mkdir = this.fs.mkdir;
+
+      this.fs.mkdir = function() {
+        test.strictEqual(arguments.length, 3);
+
+        test.strictEqual(typeof arguments[0], 'string');
+
+        test.strictEqual(typeof arguments[1], 'string');
+        test.strictEqual(arguments[1], '0755');
+
+        test.strictEqual(typeof arguments[2], 'function');
+
+        test.done();
+      };
+
+      this.cFile._writeConnectionsExistsCallback(false);
+      this.fs.mkdir = mkdir;
     }
-
-    this.cFile._writeConnections(connections, callbackFunction);
-
-    this.fs.writeFile = writeFile;
   }
 };
