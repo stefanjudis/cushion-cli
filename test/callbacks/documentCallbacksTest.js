185fbbf171227a007d0b2a5d3ee18ee00550f1fa
stefan judis <stefanjudis@gmail.com>
2013-06-05 19:38:32 +0200

    document content level fix #32

--- test/callbacks/documentCallbacksTest.js
+++ test/callbacks/documentCallbacksTest.js
@@ -9,6 +9,8 @@ module.exports = {
     this.cli = require('../../lib/cliRunner');
     this.cli.level = 'document';
 
+    this.clc = require('cli-color');
+
     callback();
   },
 
@@ -17,6 +19,107 @@ module.exports = {
     console.log = this._console;
 
     callback();
+  },
+
+  content: {
+    anErrorOccured: function(test) {
+      var cli = this.cli,
+          error = {
+        error: 'someError'
+      },
+          showError = cli.documentCallbacks.showError;
+
+      cli.documentCallbacks.showError = function() {
+        test.strictEqual(arguments.length, 1);
+
+        test.strictEqual(typeof arguments[0], 'object');
+        test.strictEqual(arguments[0], error);
+
+        test.done();
+      };
+
+      cli.documentCallbacks.content(error);
+
+      cli.documentCallbacks.showError = showError;
+    },
+    noErrorOccured: {
+      documentIsUndefined: function(test) {
+        var cli = this.cli,
+            error = null,
+            prompt = cli.prompt;
+
+        console.log = function() {
+          test.strictEqual(arguments.length, 1);
+
+          test.strictEqual(typeof arguments[0], 'string');
+          test.strictEqual(
+            arguments[0],
+            this.clc.red('\nThis property does not exist in this document.\n')
+          );
+        }.bind(this);
+
+        cli.prompt = function() {
+          test.done();
+        };
+
+        cli.documentCallbacks.content(error);
+
+        cli.prompt = prompt;
+      },
+      documentIdIsFunction: function(test) {
+        var cli = this.cli,
+            document = {
+          id: function() {}
+        },
+            error = null,
+            prompt = cli.prompt;
+
+        cli.unsavedChanges = false;
+
+        console.log = function() {
+          test.strictEqual(arguments.length, 1);
+
+          test.strictEqual(typeof arguments[0], 'string');
+          test.strictEqual(
+            arguments[0],
+            this.clc.yellow('\nProperty set.\n')
+          );
+        }.bind(this);
+
+        cli.prompt = function() {
+          test.strictEqual(cli.unsavedChanges, true);
+
+          test.done();
+        };
+
+        cli.documentCallbacks.content(error, document);
+
+        cli.prompt = prompt;
+      },
+      documentIdIsUndefined: function(test) {
+        var cli = this.cli,
+            document = {},
+            error = null,
+            pretty = console.pretty,
+            prompt = cli.prompt;
+
+        console.pretty = function() {
+          test.strictEqual(arguments.length, 1);
+
+          test.strictEqual(typeof arguments[0], 'object');
+          test.strictEqual(arguments[0], document);
+        }
+
+        cli.prompt = function() {
+          test.done();
+        };
+
+        cli.documentCallbacks.content(error, document);
+
+        cli.prompt = prompt;
+        console.pretty = pretty;
+      }
+    }
   },
 
 
